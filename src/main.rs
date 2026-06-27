f8cdbb7c7aebe6698006474674b96cc3435f68e5
pejman8gig <pejman8gig@gmail.com>
2026-06-27 10:46:52 +0330

    all rustdesk and purslane lines customized

--- src/main.rs
+++ src/main.rs
@@ -46,10 +46,10 @@ fn main() {
         -k, --key=[KEY] ''
        -s, --server=[] 'Start server'",
     );
-    let matches = App::new("rustdesk")
+    let matches = App::new("remotik")
         .version(crate::VERSION)
-        .author("Purslane Ltd<info@rustdesk.com>")
-        .about("RustDesk command line tool")
+        .author("Passak Ltd<info@rustdesk.com>")
+        .about("Remotik command line tool")
         .args_from_usage(&args)
         .get_matches();
     use hbb_common::{config::LocalConfig, env_logger::*};
